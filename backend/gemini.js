e09809ede285201f42c3ce3a3ebc352985a8b24b
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-03-07 17:04:44 +0530

    feat: gemini ai get hint service

---
+++ backend/gemini.js
@@ -0,0 +1,28 @@
+import { GoogleGenAI } from "@google/genai";
+const ai = new GoogleGenAI({
+	apiKey: process.env.GOOGLE_API_KEY,
+});
+
+export default async function getHint(question, userQuery, error) {
+	try {
+    const response = await ai.models.generateContent({
+    		model: "gemini-3-flash-preview",
+    		contents: `
+          You are a SQL tutor helping a student learn.
+          The assignment is: ${question}
+          The student's current query is: ${userQuery}
+          The error they got (if any): ${error}
+      
+          Give a helpful hint that guides them in the right direction.
+          DO NOT give the complete solution or write SQL for them.
+          Point out what concept they might be missing.
+          Keep it under 3 sentences.
+        `,
+    	});
+    	const hint = response.text;
+    	return hint;
+  } catch (error) {
+    console.log(error.message);
+    throw error.message;
+  }
+}
