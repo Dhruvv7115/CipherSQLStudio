ba84881572357b2864dc596d3db27b3129fda944
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-03-07 17:04:58 +0530

    feat: get hint route

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import { connectDB } from "./db/mainDb.js";
 import assignmentRoutes from "./routes/assignment.route.js";
 import executePgRoute from "./routes/execute.route.js";
 import tableRoutes from "./routes/table.route.js";
+import getHint from "./gemini.js";
 
 const app = express();
 
@@ -15,6 +16,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/assignments", assignmentRoutes);
 app.use("/api/execute", executePgRoute);
 app.use("/api/tables", tableRoutes);
+
+app.post("/api/hint", async (req, res) => {
+	const { question, userQuery, error } = req.body;
+	console.log(question, userQuery, error);
+	if (!question)
+		return res.status(400).json({ message: "Question is required" });
+	await getHint(question, userQuery, error)
+		.then((hint) => {
+			console.log(hint);
+			res.status(200).json({ hint });
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ message: err.message });
+		});
+});
 
 connectDB()
 	.then(() =>
