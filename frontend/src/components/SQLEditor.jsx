8afeaf691f92d8ac82692597137d277bea7c4618
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-03-07 16:02:31 +0530

    feat: sql editor component completed

--- frontend/src/components/SQLEditor.jsx
+++ frontend/src/components/SQLEditor.jsx
@@ -1,17 +1,30 @@
 import Editor from "@monaco-editor/react";
-
 import React from "react";
+import "../styles/_sql_editor.scss";
 
-const SQLEditor = () => {
+const SQLEditor = ({ query, setQuery }) => {
 	return (
-		<div>
-			<Editor
-				height="100vh"
-				language="sql"
-				defaultValue="// some comment"
-				theme="vs-dark"
-				width="50vw"
-			/>
+		<div className="sql-editor">
+			<div className="sql-editor__header">
+				<span className="sql-editor__title">SQL Editor</span>
+			</div>
+			<div className="sql-editor__monaco">
+				<Editor
+					height="100%"
+					defaultLanguage="sql"
+					language="sql"
+					value={query}
+					onChange={(val) => setQuery(val)}
+					theme="vs-dark"
+					width="100%"
+					options={{
+						fontSize: 20,
+						minimap: { enabled: false },
+						scrollBeyondLastLine: false,
+						padding: { top: 16 },
+					}}
+				/>
+			</div>
 		</div>
 	);
 };
